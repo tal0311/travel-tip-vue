d1e62d006a958f8526b8a2a453a4029f0dd328c2
tal0311 <tal.amit0311@gmail.com>
2023-11-23 03:38:07 +0200

    weather by locaation

--- src/components/dynamicCmp/LocWeather.vue
+++ src/components/dynamicCmp/LocWeather.vue
@@ -1,21 +1,30 @@
 <template>
- <section class="loc-weather">
+ <section class="loc-weather" v-if="weatherData">
   <h2>LocWeather</h2>
-  <pre>{{ props.info }}</pre>
+  <h4>Location name: {{ weatherData.name }}</h4>
+  <p>Temperature {{ weatherData.temp }} - {{ weatherData.dec }}</p>
+  <p>Humidity: {{ weatherData.humidity }}</p>
+  <p>Wind Speed: {{ weatherData.windSpeed }}</p>
  </section>
 </template>
 
 <script setup>
-import { onBeforeMount } from 'vue'
+import { weatherService } from '@/services/weather.service';
+import { ref, onBeforeMount } from 'vue'
 const props = defineProps({
  info: {
   type: Object,
  },
 })
 
+const weatherData = ref(null)
+async function getWeather() {
+ weatherData.value = await weatherService.getWeatherByLocation(props.info)
+
+}
 
 onBeforeMount(() => {
- console.log('props.info:', props.info)
+ getWeather()
 })
 
 </script>
